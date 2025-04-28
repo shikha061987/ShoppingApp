b714581f25ea64107e35fb11e1eb6c6f239a3a6c
Manish Rawat <rawatmani@gmail.com>
2025-04-28 09:55:36 -0400

    project add successfully

---
+++ src/app/app.config.ts
@@ -0,0 +1,35 @@
+/*import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+};
+*/
+
+/*
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes),provideHttpClient(),
+    
+  ]
+};
+*/
+
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { routes } from './app.routes';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes, withHashLocation()), // Provides the router with hash-based location strategy
+    provideHttpClient() // Provides HTTP client for making API requests
+  ]
+};
